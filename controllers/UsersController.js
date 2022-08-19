f6661ad1f0f615d7baef7b510ce70b1c20b6a345
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-08-19 18:45:42 +0300

    Create a new user

---
+++ controllers/UsersController.js
@@ -0,0 +1,31 @@
+import sha1 from 'sha1';
+import dbClient from '../utils/db';
+
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    const userExist = await dbClient.db
+      .collection('users')
+      .find({ email })
+      .toArray();
+
+    if (userExist.length === 0) {
+      const newUser = await dbClient.db
+        .collection('users')
+        .insertOne({ email, password: sha1(password) });
+      return res
+        .status(201)
+        .json({ id: newUser.ops[0]._id, email: newUser.ops[0].email });
+    }
+    return res.status(400).json({ error: 'Already exist' });
+  }
+}
+
+export default UsersController;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const router = express.Router();
 
+// route to AppContoller
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+
+// route to UsersController
+router.post('/users', UsersController.postNew);
 
 export default router;
