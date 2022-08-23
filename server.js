e07f50aa21b3b16d3b49af1f3517a1026a174676
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-08-23 20:55:42 +0300

    add file limit

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
 app.use(router);
 
 app.listen(port, () => {
