437aadbbfcc89f55b143bbfdee47fea6530d0c4c
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-08-24 13:57:32 +0300

    fix spelling error

--- test/utils/db.test.js
+++ test/utils/db.test.js
@@ -2,27 +2,27 @@
 /* eslint-disable jest/prefer-expect-assertions */
 /* eslint-disable jest/valid-expect */
 import { expect } from 'chai';
-import dbClinet from '../../utils/db';
+import dbClient from '../../utils/db';
 
 describe('dbClient', () => {
   before((done) => {
     Promise.all([
-      dbClinet.db.collection('files').deleteMany({}),
-      dbClinet.db.collection('users').deleteMany({}),
+      dbClient.db.collection('files').deleteMany({}),
+      dbClient.db.collection('users').deleteMany({}),
     ])
       .then(() => done())
       .catch((err) => done(err));
   });
 
   it('dbClient is Alive', () => {
-    expect(dbClinet.isAlive()).to.equal(true);
+    expect(dbClient.isAlive()).to.equal(true);
   });
 
   it('returns number of users', async () => {
-    expect(await dbClinet.nbUsers()).to.equal(0);
+    expect(await dbClient.nbUsers()).to.equal(0);
   });
 
   it('returns number of files', async () => {
-    expect(await dbClinet.nbFiles()).to.equal(0);
+    expect(await dbClient.nbFiles()).to.equal(0);
   });
 });
